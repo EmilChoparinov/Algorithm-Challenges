5078376ad37f56509ecd011460fb1b9bb2ce97d2
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-24 15:57:44 -0800

    dont worry be happy

---
+++ chapter-1/dont-worry-be-happy.js
@@ -0,0 +1,11 @@
+/**
+ * Prints good morning 98 times
+ * @returns good morning!
+ */
+function beCheerful(){
+    for(let i = 0; i < 98; i++){
+        console.log(`good morning for the ${i+1} time!`)
+    }
+}
+
+beCheerful()
