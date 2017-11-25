2d9876da7d36065d4359895e6ea9f4342afbc0c3
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-24 17:31:55 -0800

    celsius to fahrenheit

---
+++ chapter-1/page-20/celsius-to-fahrenheit.js
@@ -0,0 +1,8 @@
+/**
+ * Convert from celsius to fahrenheit
+ * @param {Number} cDegrees degress in celcius
+ * @returns {Number} fahrenheit degress
+ */
+function celsiusToFahrenheit(cDegrees){
+    return (cDegrees * 9/5) + 32;
+}
