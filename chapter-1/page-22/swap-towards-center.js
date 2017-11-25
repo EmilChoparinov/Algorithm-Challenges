d43d66e9b06670d1a57ad85f93da0c967fbdee3d
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-24 20:19:43 -0800

    swap toward the center

---
+++ chapter-1/page-22/swap-towards-center.js
@@ -0,0 +1,14 @@
+function swapTowardCenter(arr) {
+    if (arr instanceof Array) {
+        for (let i = 0; i < arr.length / 2; i++) {
+            if (i % 2 == 0) {
+                let temp = arr[i];
+                arr[i] = arr[arr.length - 1 - i];
+                arr[arr.length - 1 - i] = temp;
+            }
+        }
+        return arr;
+    }
+}
+console.log(swapTowardCenter([true, 42, 'ada', 2, 'pizza']))
+console.log(swapTowardCenter([1, 2, 3, 4, 5, 6]))
