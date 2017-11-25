2e67c6f387619afe50f5900986d929f886e393ea
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-24 18:49:54 -0800

    previous lengths

--- chapter-1/page-22/previous-lengths.js
+++ chapter-1/page-22/previous-lengths.js
@@ -2,15 +2,16 @@
  * Replace each string with a number which is the length of the string at the previous array index
  * @param {Array|String} arr an array of strings
  */
-function previousLengths(arr){
-    if(arr instanceof Array){
-        for(let i = 1; i < arr.length; i++){
-            if(typeof arr[i] == 'string'){
-                arr[i-1] = arr[i].length;
-            }
+function previousLengths(arr) {
+    if (arr instanceof Array) {
+        let lastStr = arr[0];
+        for (let i = 1; i < arr.length; i++) {
+            let curStr = arr[i];
+            arr[i] = lastStr.length;     
+            lastStr = curStr;   
         }
     }
     return arr;
 }
 
-console.log(previousLengths(['a', 2, 'abc','abcd']))
+console.log(previousLengths(['a', 'ab', 'abc', 'abcd']))
