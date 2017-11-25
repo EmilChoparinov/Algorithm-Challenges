785db5029ec4ea5989a493acd51526248004cd4f
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-24 17:43:14 -0800

    make biggie

---
+++ chapter-1/page-22/biggie-size.js
@@ -0,0 +1,17 @@
+/**
+ * A funciton that changes all positive numbers in the array to 'big'
+ * @param {Number} arr an array
+ */
+function makeBiggie(arr){
+    if(arr instanceof Array){
+        for(let i = 0; i < arr.length; i++){
+            if(arr[i] > 0){
+                arr[i] = "big"
+            }
+        }
+        return arr;
+    }
+    return null;
+}
+
+console.log(makeBiggie([-1,3,5,-5]))
