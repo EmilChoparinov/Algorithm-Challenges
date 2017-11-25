6a7dd83e33a9665b4b314fbac2a1d27e81a76c2b
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-24 17:48:20 -0800

    print low return high

--- chapter-1/page-22/print-low-return-high.js
+++ chapter-1/page-22/print-low-return-high.js
@@ -1,3 +1,19 @@
-// function printLowReturnHigh(arr){
-//     if(arr instanceof Array)
-// }
+/**
+ * A function that prints the lowest value in the array and returns the highest value in the array
+ * @param {Array} arr an array
+ */
+function printLowReturnHigh(arr){
+    if(arr instanceof Array){
+        let lowestNum = arr[0];
+        let highestNum = arr[0];
+        for(let i = 1; i < arr.length; i++){
+            if(lowestNum > arr[i]) lowestNum = arr[i];
+            if(highestNum < arr[i]) highestNum = arr[i];
+        }
+        console.log(lowestNum);
+        return highestNum;
+    }
+    return null;
+}
+
+console.log(printLowReturnHigh([1,-6,3,45,8,6,4,4]))
