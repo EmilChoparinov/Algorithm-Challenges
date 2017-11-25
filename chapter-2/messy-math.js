4662d24013a2c03443eaf538a4505d707ffa1278
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-25 01:11:32 -0800

    messy math

---
+++ chapter-2/messy-math.js
@@ -0,0 +1,21 @@
+function messyMath(num){
+    let sum = 0;
+    for(let i = 0; i <= num; i++){
+        if(i % 3 == 0){
+            continue;
+        }
+        else if(i % 7 == 0){
+            sum += i * 2;
+        }
+        else if(num/3 == i){
+            return -1;
+        }else{
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+console.log(messyMath(4))
+console.log(messyMath(8))
+console.log(messyMath(15))
