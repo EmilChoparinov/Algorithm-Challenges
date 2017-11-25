68ca958b2020a180701ebce34ae72a3553703745
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-24 16:30:00 -0800

    what do you know

---
+++ chapter-1/what-do-you-know.js
@@ -0,0 +1,9 @@
+/**
+ * Given an input parameter incomming. Please console.log this value
+ * @param {Object} incomming just something to print
+ */
+function whatDoYouKnow(incomming){
+    console.log(incomming)
+}
+
+whatDoYouKnow("about")
