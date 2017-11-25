12698a89349f9f993adce6ecea7d045c54e8a12b
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-25 00:11:58 -0800

    factorial

---
+++ chapter-2/factorial.js
@@ -0,0 +1,10 @@
+function factorial(num){
+    let fact = 1;
+    for(let i = 2; i <= num; i++){
+        fact *= i;
+    }
+    return fact;
+}
+
+console.log(factorial(3))
+console.log(factorial(5))
