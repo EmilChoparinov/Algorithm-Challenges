dc1766b64ee3d444966c7cc10b7d175c359a0903
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-24 17:04:26 -0800

    print and return

---
+++ chapter-1/page-20/print-and-return.js
@@ -0,0 +1,10 @@
+/**
+ * Print the first value, return the second
+ * @param {Array} arr array of two numbers
+ */
+function printAndReturn(arr){
+    console.log(arr[0]);
+    return arr[1];
+}
+
+console.log(printAndReturn([1,2]))
