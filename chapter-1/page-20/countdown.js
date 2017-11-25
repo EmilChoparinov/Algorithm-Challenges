6fa06e0199bb60d92f11e5386913b98f012d4a44
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-24 17:02:23 -0800

    countdown

--- chapter-1/page-20/countdown.js
+++ chapter-1/page-20/countdown.js
@@ -1,6 +1,7 @@
 /**
  * Create a function that accepts a number as an input. Return a new array that counts down by one, from the number down to 0.
  * @param {Number} num starting number of the array
+ * @returns {Number|Array} array of all numbers from num to 0 
  */
 function countDown(num){
     let arr = []
@@ -11,4 +12,4 @@ function countDown(num){
 }
 
 // Who long is the array? length = num + 1
-console.log(countDown(50).length)
+console.log(countDown(50))
