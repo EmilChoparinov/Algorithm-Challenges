b4663dc0cc449f689926e19b111f3ba2d5184ea8
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-24 17:07:58 -0800

    first plus length

---
+++ chapter-1/page-20/first-plus-length.js
@@ -0,0 +1,10 @@
+/**
+ * Returns the sum of the first value in the array plus the array's length.
+ * @param {Number} arr arr of length > 1
+ * @returns {Object}
+ */
+function firstPlusLength(arr){
+    return arr[0]+arr.length;
+}
+
+console.log(firstPlusLength([1,2,3]))
