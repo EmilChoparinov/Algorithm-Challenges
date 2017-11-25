00d5e5a814dffacadab860c196f468dd8dd6d4cf
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-24 23:59:32 -0800

    shift array values left

---
+++ the-basic-13/shift-array-values-left.js
@@ -0,0 +1,14 @@
+function shiftArrayValsLeft(arr){
+    if(arr instanceof Array){
+        let lastNum = arr[arr.length - 1]
+        for(let i = arr.length - 1; i > 0; i--){
+            let curNum = arr[i-1];
+            arr[i-1] = lastNum;
+            lastNum = curNum;
+        }
+        arr[arr.length-1] = 0;
+        return arr;
+    }
+}
+
+console.log(shiftArrayValsLeft([12,123,5,2,3,5,1]))
