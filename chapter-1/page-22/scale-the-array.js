4e4d72c9acf485b9f3646d875b41517fb4f0a021
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-24 20:22:56 -0800

    scale the array

--- chapter-1/page-22/scale-the-array.js
+++ chapter-1/page-22/scale-the-array.js
@@ -1,8 +1,15 @@
+/**
+ * Scales the array by a mulitplers
+ * @param {Array} arr an array of numbers
+ * @param {Number} num a number of which the array should be scaled by
+ */
 function scaleArray(arr, num){
     if(arr instanceof Array){
         for(let i = 0; i < arr.length; i++){
             arr[i] *= num;
         }
-        return num;
+        return arr;
     }
 }
+
+console.log(scaleArray([1,2,3,4,5], 3))
