4edb83a76d1693c8d09db083c6a6f0269e096207
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-25 22:14:32 -0800

    nth largest

--- chapter-3/nth-to-last.js
+++ chapter-3/nth-to-last.js
@@ -1,3 +1,8 @@
+/**
+ * Finds the nth last element
+ * @param {Array} arr 
+ * @param {Number} order order of which index the element should be taken from
+ */
 function nthToLast(arr, order){
     if(arr instanceof Array){
         if(order > arr.length) return null;
