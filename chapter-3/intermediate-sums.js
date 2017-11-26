a1c60bc24a20014d9fd81535c8908c25d611dadd
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-25 23:29:31 -0800

    intermediate sum

---
+++ chapter-3/intermediate-sums.js
@@ -0,0 +1,25 @@
+function intermediateSums(arr) {
+    if (arr instanceof Array) {
+        let addSum = function (index, sum){
+            if(typeof index == 'number' && typeof sum == 'number'){
+                for(let i = arr.length - 1; i >= index; i--){
+                    arr[i+1] = arr[i];
+                }
+                arr[index] = sum;
+            }
+        }
+        let sum = 0;
+        for(let i = 0; i < arr.length; i++){
+            if(i % 10 == 0 && i > 0){
+                addSum(i, sum);
+                sum = 0;
+            }else{
+                sum += arr[i];
+            }
+        }
+        addSum(arr.length, sum);
+        return arr;
+    }
+}
+
+console.log(intermediateSums([1,2,1,2,1,2,1,2,1,2,1,2,1,2]))
