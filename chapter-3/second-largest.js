80d07c7942e5ecdfba9ca64cb782c327374e2ea1
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-25 22:07:44 -0800

    second largest

---
+++ chapter-3/second-largest.js
@@ -0,0 +1,25 @@
+/**
+ * Finds the second max
+ * @param {Array|Number} arr an array of numbers
+ */
+function secondLargest(arr) {
+    if (arr instanceof Array) {
+        let max = [arr[0],0];
+        for(let i = 0; i < arr.length; i++){
+            if(arr[i] > max[0]){
+                max[0] = arr[i];
+                max[1] = i;
+            } 
+        }
+        arr[max[1]] = -Infinity;
+        let nextMax = arr[0];
+        for(let i = 0; i < arr.length; i++){
+            if(max[0] - arr[i] < max[0] - nextMax && arr[i] != max[0]){
+                nextMax = arr[i];
+            }
+        }
+        return nextMax;
+    }
+}
+
+console.log(secondLargest([1, 42, 4, Math.PI, 7]))
