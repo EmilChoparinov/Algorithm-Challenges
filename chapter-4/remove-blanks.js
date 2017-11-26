02e063a434803572091aa7c1ad0fde2098c661bc
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-26 15:31:50 -0800

    remove blanks

---
+++ chapter-4/remove-blanks.js
@@ -0,0 +1,11 @@
+function removeBlanks(str){
+    if(typeof str == 'string'){
+        let noSpace = "";
+        for(let i = 0; i < str.length; i++){
+            if(str[i] != ' ') noSpace += str[i];
+        }
+        return noSpace;
+    }
+}
+
+console.log(removeBlanks(' Pl   ayTha   tF  un kyM  u s ic'))
