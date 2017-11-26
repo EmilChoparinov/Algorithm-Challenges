d58de3c7a37e1f7e3de3f8913dcd58d0d73ed1ae
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-25 22:52:19 -0800

    credit card validation

--- chapter-3/credit-card-validation.js
+++ chapter-3/credit-card-validation.js
@@ -1,5 +1,5 @@
 function luhnFormula(arr){
-    if(arr instanceof Array){
+    if(arr instanceof Array || arr instanceof String){
         let sum = 0;
         for(let i = arr.length - 2; i >= 0; i -= 2){
             arr[i] *= 2;
@@ -7,7 +7,6 @@ function luhnFormula(arr){
             sum += arr[i] + arr[i-1];
         }
         sum += arr[arr.length - 1];
-        console.log(arr);
         return (sum % 10 == 0)
     }
 }
