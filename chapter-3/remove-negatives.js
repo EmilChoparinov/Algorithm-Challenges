ed9e521fdfca01a46b35e868c8d47b91e2decc90
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-25 21:41:48 -0800

    remove negatives

---
+++ chapter-3/remove-negatives.js
@@ -0,0 +1,16 @@
+function removeNegatives(arr){
+    if(arr instanceof Array){
+        let newArr = []
+        for(let i = 0; i < arr.length; i++){
+            if(arr[i] >= 0){
+                newArr.length++;
+                newArr[newArr.length - 1] = arr[i];
+            }
+        }
+        return newArr;
+    }
+}
+
+var array = [1,2,3,4,5,6,7,-1,-2,-3,8,9,-10,10]
+array = removeNegatives(array);
+console.log(array)
