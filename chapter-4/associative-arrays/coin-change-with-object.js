af9afbd4dad80334fa69a9467e96be2fafd519a8
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-28 16:14:13 -0800

    coin change

---
+++ chapter-4/associative-arrays/coin-change-with-object.js
@@ -0,0 +1,21 @@
+/**
+ * Representation of the smallest amount of coins
+ * @param {Number} num coins total in pennies
+ */
+function coinChange(num) {
+    var ideal = {
+        'quarters': 0,
+        'dimes': 0,
+        'nickles': 0,
+        'pennies': 0,
+    };
+
+    ideal['quarters'] = (num - num % 25) / 25;
+    num = num % 25;
+    ideal['dime'] = (num - num % 10) / 10;
+    num = num % 10;
+    ideal['nickles'] = (num - num % 5) / 5;
+    ideal['pennies'] = num % 5;
+
+    return ideal;
+}
