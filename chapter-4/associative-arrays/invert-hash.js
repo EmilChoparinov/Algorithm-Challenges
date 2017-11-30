7934217bb946d9aea28646197ba9624da4b9a35e
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-29 17:43:59 -0800

    invert hash

---
+++ chapter-4/associative-arrays/invert-hash.js
@@ -0,0 +1,17 @@
+function invertHash(assocArr){
+    for(let i in assocArr){
+        console.log(assocArr[i]);
+        let temp = assocArr[i];
+        delete assocArr[i];
+        assocArr[temp] = i;
+    }
+    return assocArr;
+}
+
+var obj = {
+    "name": "zaphod",
+    "charm": "high",
+    "morals": "dicey",
+}
+
+console.log(invertHash(obj));
