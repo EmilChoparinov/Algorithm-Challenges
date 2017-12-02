454e9cbd3b24f1a5fa6682a621836ebc15af805a
EmilChoparinov <emilchoparinov@gmail.com>
2017-12-02 13:39:51 -0800

    remove back

---
+++ chapter-5/remove-back.js
@@ -0,0 +1,59 @@
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    /**
+     * Adds a value to the linked list
+     * @param {Object} val 
+     */
+    add(val) {
+        var lastNode = this.head;
+        var addNode = new node(val);
+        this.head = addNode;
+        addNode.next = lastNode;
+        if (this.back == null) this.back = addNode;
+        return this;
+    }
+    
+    /**
+     * returns a formatted string of the list
+     */
+    toString() {
+        let node = this.head;
+        let s = "[ "
+        while (node != null) {
+            if(node.next == null) break;
+            s += node.val + ", ";
+            node = node.next;
+        }
+        s += node.val + " ]";
+        return s;
+    }
+
+    removeBack(){
+        let node = this.head;
+        while(node != null){
+            if(node.next != null){
+                if(node.next.next == null){
+                    break;
+                }
+            }
+            node = node.next;
+        }
+        node.next = null;
+        return this;
+    }
+}
+
+class node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+var list = new LinkedList();
+list.add(1).add(2).add(3).add(4);
+list.removeBack().removeBack();
+console.log(list.toString());
