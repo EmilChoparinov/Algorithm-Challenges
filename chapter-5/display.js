9832aa1424d8b4beb3f49922794b5d6479aeec73
EmilChoparinov <emilchoparinov@gmail.com>
2017-12-02 13:20:52 -0800

    display

---
+++ chapter-5/display.js
@@ -0,0 +1,45 @@
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.back = null;
+    }
+
+    /**
+     * Adds a value to the linked list
+     * @param {Object} val 
+     */
+    add(val) {
+        var lastNode = this.head;
+        var addNode = new node(val);
+        this.head = addNode;
+        addNode.next = lastNode;
+        if (this.back == null) this.back = addNode;
+        return this;
+    }
+    
+    /**
+     * returns a formatted string of the list
+     */
+    display() {
+        let node = this.head;
+        let s = "[ "
+        while (node != null) {
+            if(node.next == null) break;
+            s += node.val + ", ";
+            node = node.next;
+        }
+        s += node.val + " ]";
+        return s;
+    }
+}
+
+class node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+let list = new LinkedList();
+list.add(1).add(2).add(3);
+console.log(list.display());
