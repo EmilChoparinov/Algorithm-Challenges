f856da2ef1234bfe1a0725830ad420b8ae90895f
EmilChoparinov <emilchoparinov@gmail.com>
2017-12-07 19:39:23 -0800

    push

---
+++ chapter-6/all-arr-stack.js
@@ -0,0 +1,13 @@
+class ArrStack{
+    constructor(){
+        this.stack = [];
+    }
+    /**
+     * Add a value to the stack
+     * @param {Any} val 
+     */
+    push(val){
+        this.stack[this.stack.length] = val;
+        return this;
+    }
+}
