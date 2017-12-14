07d92e54f6d6ae208819a83956be62be7824dd1a
EmilChoparinov <emilchoparinov@gmail.com>
2017-12-14 11:03:35 -0800

    average

---
+++ chapter-7/average.js
@@ -0,0 +1,10 @@
+function average(arr){
+    if(arr instanceof Array){
+        let s = 0;
+        arr.forEach(element => s += element);
+        return s/arr.length;
+    }
+}
+
+var arr = [1,2,3,4,5,6,7,8];
+console.log(average(arr));
