fe3191963ec1a62cca91c937aa29286ae6a8a78b
EmilChoparinov <emilchoparinov@gmail.com>
2017-12-14 12:43:57 -0800

    balance point

---
+++ chapter-7/balance-point.js
@@ -0,0 +1,30 @@
+function balancePoint(arr){
+    if(arr instanceof Array){
+        let sumLeft = arr[0];
+        let sumRight = arr[arr.length-1];
+        let c = false;
+        for(let i = 1; i < arr.length-2; i++){
+            if(sumLeft <= sumRight){
+                sumLeft += arr[i];
+            }
+            else if(sumLeft > sumRight){
+                c = !c;
+                sumRight += arr[arr.length - i];
+                i--;
+            }
+        }
+        console.log(sumLeft, sumRight);
+        if(!c){
+            if(sumLeft + arr[arr.length-2] == sumRight) return true;
+        }
+        if(sumLeft != sumRight) return false;
+        return true;
+    }
+}
+
+var arr = [1,2,3,4,10];
+console.log(balancePoint(arr));
+
+
+var arr = [1,2,4,6,1];
+console.log(balancePoint(arr));
