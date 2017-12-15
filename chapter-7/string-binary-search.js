e28702920b712395fdbbabf23bd1804247160ec4
EmilChoparinov <emilchoparinov@gmail.com>
2017-12-14 19:02:42 -0800

    string binary search

--- chapter-7/string-binary-search.js
+++ chapter-7/string-binary-search.js
@@ -3,9 +3,9 @@ function binarySearch(str, char){
         let l = 0;
         let r = str.length - 1;
         while(l < r){
-            let mid = Math.floor((l+r)/2);
-            if(str[l] == char) return true;
-            if(str.charCodeAt(mid) > char){
+            let mid = Math.floor((l+r)/2+1);
+            if(str[mid] == char) return true;
+            if(str.charCodeAt(mid) < char){
                 l = mid+1;
             }else{
                 r = mid-1;
@@ -16,4 +16,5 @@ function binarySearch(str, char){
 }
 
 var str = "&-0379DEFXA[abcz|"
-console.log(binarySeach(str, "6"));
+console.log(binarySearch(str, "6"));
+console.log(binarySearch(str, "0"));
