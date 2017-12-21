928c8ee04ac64d2505ce5363e3ee44623d46d9f3
EmilChoparinov <emilchoparinov@gmail.com>
2017-12-21 11:36:01 -0800

    shift right and left list

--- chapter-8/shift-right.js
+++ chapter-8/shift-right.js
@@ -2,21 +2,40 @@ const reference = require("./reference-class");
 let Node = reference.Node;
 let LinkedList = reference.LinkedList;
 
+/**
+ * Shifts a list to the right by a certain value
+ * @param {LinkedList} list 
+ * @param {Number} shiftBy 
+ */
 function shiftRight(list, shiftBy) {
-    if (list instanceof LinkedList && typeof shiftBy == 'number' && shiftBy > 0) {
+    if (list instanceof LinkedList && typeof shiftBy == 'number') {
         let node1 = list.head;
         let node2 = list.head;
         let lastNode = list.head;
-        while (node1) {
-            if (shiftBy == 0) {
-                node2 = list.head;
+        if (shiftBy >= 0) {
+            while (node1) {
+                if (shiftBy == 0) {
+                    node2 = list.head;
+                }
+                if (!node1.next) {
+                    lastNode = node1;
+                }
+                shiftBy--;
+                node1 = node1.next;
+                node2 = node2.next;
             }
-            if (!node1.next) {
-                lastNode = node1;
+        }
+        else {
+            while (node1) {
+                if (shiftBy < 0) {
+                    node2 = node2.next;
+                }
+                if (!node1.next) {
+                    lastNode = node1;
+                }
+                shiftBy++;
+                node1 = node1.next;
             }
-            shiftBy--;
-            node1 = node1.next;
-            node2 = node2.next;
         }
         let lastFragment = list.head;
         while (lastFragment) {
@@ -34,4 +53,6 @@ let list = new LinkedList();
 list.add(1).add(2).add(3).add(4);
 console.log(list.toString());
 shiftRight(list, 1);
+console.log(list.toString());
+shiftRight(list, -1);
 console.log(list.toString());
