c42eae41ab8e4963e7e78cccfc1db60b91f082f1
EmilChoparinov <emilchoparinov@gmail.com>
2017-12-21 16:59:59 -0800

    has loop

---
+++ chapter-8/has-loop.js
@@ -0,0 +1,37 @@
+const reference = require("./reference-class");
+let Node = reference.Node;
+let LinkedList = reference.LinkedList;
+
+/**
+ * Finds out if a linked list loops somewhere
+ * @param {LinkedList} list 
+ */
+function hasLoop(list) {
+    if (list instanceof LinkedList) {
+        let node1 = list.head;
+        let node2 = list.head;
+        while (node2 && node1) {
+            if(node2.val == node1.val) return true;
+            node1 = node1.next;
+            node2 = node2.next;
+            if (node2) {
+                node2 = node2.next;
+            } else {
+                break;
+            }
+        }
+        return false;
+    }
+    return false;
+}
+
+let list = new LinkedList();
+list.add(1).add(2).add(3).add(4);
+let node = list.head.next;
+let end = list.head;
+while(end){
+    if(!end.next) break;
+    end = end.next;
+}
+end.next = node;
+console.log(hasLoop(list));
