388716e046edf5ac570475510339f0c0a493d010
EmilChoparinov <emilchoparinov@gmail.com>
2017-12-24 16:39:46 -0800

    repair list

---
+++ chapter-8/doubly-linked-lists/repair.js
@@ -0,0 +1,28 @@
+const reference = require("./reference-class");
+const validity = require("./is-valid");
+let Node = reference.Node;
+let DLinkedList = reference.DLinkedList;
+let loopify = validity.loopify;
+
+function repair(list) {
+    if(list instanceof DLinkedList){
+        if(list.tail){
+            if(list.tail.next){
+                list.tail.next = null;
+            }
+        }
+        let node = list.head;
+        while(node){
+            if(node.next == node.prev){
+                node.next = null;
+            }
+            node = node.next;
+        }
+    }
+}
+
+let list = new DLinkedList();
+list.push("a").push("b").push("c").push("d");
+loopify(list, 1);
+repair(list);
+console.log(list.toString());
