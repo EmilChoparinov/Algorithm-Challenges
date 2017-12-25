c348cbf8130ae25eb7d07812a7e18f141d105054
EmilChoparinov <emilchoparinov@gmail.com>
2017-12-24 19:10:36 -0800

    in order subsets

---
+++ chapter-9/in-order-subsets.js
@@ -0,0 +1,8 @@
+function inOrderSubsets(str, aux = "", subsets = []) {
+    if (str.length <= 0) return subsets.push(aux);
+    inOrderSubsets(str.slice(1), aux.concat(str[0]), subsets);
+    inOrderSubsets(str.slice(1), aux, subsets);
+    return subsets;
+};
+
+console.log(inOrderSubsets('abc'))
