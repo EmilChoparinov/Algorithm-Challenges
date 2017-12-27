8cdd41c5d88c5c017b1722d0431d46b7b49ff9a2
EmilChoparinov <emilchoparinov@gmail.com>
2017-12-26 19:24:13 -0800

    unique letters

---
+++ chapter-10/unique-letters.js
@@ -0,0 +1,19 @@
+function uniqueLetters(str) {
+    if (typeof str == 'string') {
+        let set = {};
+        for (let i = 0; i < str.length; i++) {
+            if (str[i] != "." || str[i] != "!" || str[i] != ",") {
+                if (!set[str[i]]) set[str[i]] = 1;
+                else set[str[i]] += 1;
+            }
+        }
+        console.log(set);
+        let s = "";
+        for (let i = 0; i < str.length; i++) {
+            if (set[str[i]] == 1) s += str[i];
+        }
+        return s;
+    }
+}
+
+console.log(uniqueLetters("Snap! Crackle! Poop!"));
