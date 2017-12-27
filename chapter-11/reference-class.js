e29a6496ee3143cf961e59f0403b05c16844e729
EmilChoparinov <emilchoparinov@gmail.com>
2017-12-27 15:54:22 -0800

    add

--- chapter-11/reference-class.js
+++ chapter-11/reference-class.js
@@ -12,10 +12,36 @@ class BST {
     }
 
     add(val) {
-
+        let node = this.root;
+        let newNode = new BTNode(val);
+        if (!node) {
+            this.root = newNode;
+            return this;
+        }
+        while (node) {
+            if (val > node.val) {
+                if (node.right) {
+                    node = node.right;
+                } else {
+                    node.right = newNode;
+                    break;
+                }
+            }
+            else {
+                if (node.left) {
+                    node = node.left;
+                } else {
+                    node.left = newNode;
+                    break;
+                }
+            }
+        }
+        return this;
     }
-    contains(val) {
 
+
+    contains(val) {
+        
     }
     min() {
 
@@ -30,3 +56,5 @@ class BST {
 
     }
 }
+
+let tree = new BST();
