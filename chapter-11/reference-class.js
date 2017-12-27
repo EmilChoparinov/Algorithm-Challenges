351b11de4bba7be2a28d8cbf47fe9391a72465c5
EmilChoparinov <emilchoparinov@gmail.com>
2017-12-27 15:46:50 -0800

    base class

---
+++ chapter-11/reference-class.js
@@ -0,0 +1,32 @@
+class BTNode {
+    constructor(value) {
+        this.val = value;
+        this.right = null;
+        this.left = null;
+    }
+}
+
+class BST {
+    constructor() {
+        this.root = null;
+    }
+
+    add(val) {
+
+    }
+    contains(val) {
+
+    }
+    min() {
+
+    }
+    max() {
+
+    }
+    size() {
+
+    }
+    isEmpty() {
+
+    }
+}
