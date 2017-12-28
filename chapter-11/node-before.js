853c0d88f729a80b05bd680ce5b5c7fea2409fc1
EmilChoparinov <emilchoparinov@gmail.com>
2017-12-28 15:51:11 -0800

    val before

--- chapter-11/node-before.js
+++ chapter-11/node-before.js
@@ -2,16 +2,16 @@ const reference = require("./reference-class");
 var BST = reference.BST;
 var BTNode = reference.BTNode;
 
-function valBefore(node, val) {
+function nodeBefore(node, val) {
     if (!node) return null;
     //special case q is for if the traversal never reached going left
-    let q = valBefore(node.right, val);
+    let q = nodeBefore(node.right, val);
     if (q) return q;
-    if (node.val < val) return node.val;
-    else return valBefore(node.left, val);
+    if (node.val < val) return node;
+    else return nodeBefore(node.left, val);
 }
 
 let tree = new BST();
 tree.add(60).add(41).add(18).add(25).add(53).add(48).add(42).add(55);
 tree.add(74).add(85).add(63).add(62).add(64).add(70);
-console.log(valBefore(tree.root, 65));
+console.log(nodeBefore(tree.root, 65));
