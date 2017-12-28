b8cbe25ef2a667ad763af600ce1384f4dd36342d
EmilChoparinov <emilchoparinov@gmail.com>
2017-12-28 15:53:42 -0800

    node before

--- chapter-11/node-before.js
+++ chapter-11/node-before.js
@@ -14,4 +14,4 @@ function nodeBefore(node, val) {
 let tree = new BST();
 tree.add(60).add(41).add(18).add(25).add(53).add(48).add(42).add(55);
 tree.add(74).add(85).add(63).add(62).add(64).add(70);
-console.log(nodeBefore(tree.root, 65));
+console.log(nodeBefore(tree.root, 22));
