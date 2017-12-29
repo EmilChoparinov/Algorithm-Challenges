7dc56bf731ae5732966a62e307e6772b9b2ed6b3
EmilChoparinov <emilchoparinov@gmail.com>
2017-12-29 13:05:19 -0800

    selection sort and prepared sort tester for linked lists

---
+++ chapter-12/selection-sort.js
@@ -0,0 +1,27 @@
+const testSort = require("./sort-tester");
+
+function selectionSort(arr) {
+    if (arr instanceof Array) {
+        for (let i = 0; i < arr.length; i++) {
+            let n = getCurMinIndexAfter(arr, i);
+            let t = arr[i];
+            arr[i] = arr[n];
+            arr[n] = t;
+        }
+        return arr;
+    }
+}
+
+function getCurMinIndexAfter(arr, n) {
+    let curMin = n;
+    let curMinVal = arr[n];
+    for (let i = n; i < arr.length; i++) {
+        if (curMinVal > arr[i]) {
+            curMin = i;
+            curMinVal = arr[i];
+        }
+    }
+    return curMin;
+}
+
+testSort(selectionSort);
--- chapter-12/sort-tester.js
+++ chapter-12/sort-tester.js
@@ -1,3 +1,7 @@
+const reference = require("../chapter-8/singly-linked-lists/reference-class");
+var Node = reference.Node;
+var LinkedList = reference.LinkedList;
+
 let arrays = [
     [1, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48],
     [87, 97, 70, 93, 97, 78, 85, 20, 9, 77, 68, 93, 58, 34, 51],
@@ -6,8 +10,38 @@ let arrays = [
     [96, 53, 99, 100, 33, 51, 76, 9, 29, 89, 31, 91, 30, 94, 90]
 ];
 
-module.exports = function testSort(callback) {
+module.exports = function testSort(sortType, callback) {
     let attempts = [];
+    if (sortType == 'linked') {
+        let linkedLists = [];
+        arrays.forEach(array => {
+            let list = new LinkedList();
+            array.forEach(value => list.add(value));
+            attempts.push(list);
+            linkedLists.push(list);
+        });
+        attempts.forEach(attempt => {
+            if (attempt instanceof LinkedList) {
+                console.log("currently attempting... ", attempt.toString());
+                let node1 = attempt.head;
+                if (node1) {
+                    let node2 = node1.next;
+                    while (node2) {
+                        if (node1.val > node2.val) {
+                            console.log(`FAIL @ ${node1.val} -> ${node2.val}`);
+                            return;
+                        }
+                        node1 = node1.next;
+                        node2 = node2.next;
+                    }
+                }
+                console.log("SUCCESS");
+            }
+        });
+    }
+    if (sortType == 'array') {
+
+    }
     arrays.forEach(array => attempts.push(callback(array)));
     attempts.forEach(attempt => {
         console.log("currently attempting... ", attempt);
