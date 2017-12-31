b91981db5483a80b1d69490ab67bf74c4871e27e
EmilChoparinov <emilchoparinov@gmail.com>
2017-12-30 19:05:13 -0800

    pancake sort

--- chapter-12/pancake-sort.js
+++ chapter-12/pancake-sort.js
@@ -1,17 +1,23 @@
+const testSort = require("./sort-tester");
+
 function pancakeSort(arr) {
     if (arr instanceof Array) {
-        for (let i = arr.length - 1; i >= 1; i--) {
+        for (let i = arr.length - 1; i > 0; i--) {
             var max = 0;
             for (let j = 1; j <= i; j++) {
                 if (arr[max] < arr[j]) {
                     max = j;
                 }
             }
-
             if (max != i) {
                 let t = arr.slice(0, max + 1).reverse();
                 for (let k = 0; k < max + 1; k++) {
                     arr[k] = t[k];
+                }
+
+                let finishingFlip = arr.slice(0, i + 1).reverse();
+                for (let k = 0; k <= i; k++) {
+                    arr[k] = finishingFlip[k];
                 }
             }
         }
@@ -19,5 +25,4 @@ function pancakeSort(arr) {
     }
 }
 
-let arr = [2, 3, 1, 4];
-console.log(pancakeSort(arr));
+testSort('array', pancakeSort);
