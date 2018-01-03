96a29aee63b5b41e0ecd9d50005d3110344a8f85
EmilChoparinov <emilchoparinov@gmail.com>
2018-01-02 16:22:54 -0800

    union unsorted arrays

---
+++ chapter-13/union-unsorted-arrays.js
@@ -0,0 +1,18 @@
+function unionUnsortedArrays(a, b) {
+    if (a instanceof Array && b instanceof Array) {
+        let ai = 0, bi = 0, arr = [];
+        while (ai < a.length && bi < b.length) {
+            if (a[ai] == b[bi]) {
+                arr.push(a[ai]);
+            } else {
+                arr.push(a[ai]);
+                arr.push(b[bi]);
+            }
+            ai++;
+            bi++;
+        }
+        return arr;
+    }
+}
+
+console.log(unionUnsortedArrays([2, 7, 2, 1, 2], [6, 7, 2, 7, 6, 2]));
