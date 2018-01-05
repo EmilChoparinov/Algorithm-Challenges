1c6e0c35a2e71e624f48ea6ccda9c63ca8ed4941
EmilChoparinov <emilchoparinov@gmail.com>
2018-01-04 17:55:14 -0800

    select n keys, added extra dim for hash collisions

--- chapter-14/hashmap.js
+++ chapter-14/hashmap.js
@@ -1,8 +1,28 @@
 class HashMap {
     constructor(capactiy) {
-        this.capactiy = capactiy;
         this.table = [];
+        capactiy = this.closestLargestPrime(capactiy)
+        this.capactiy = capactiy;
+        this.table.length = capactiy;
         this.elements = 0;
+    }
+
+    closestLargestPrime(n) {
+        while (!this.isPrime(n)) {
+            n++;
+        }
+        return n;
+    }
+
+    isPrime(n) {
+        if (typeof n == 'number') {
+            if (n == 2) return true;
+            if (n % 2 == 0 || n <= 1) return false;
+            let sqrtNum = Math.sqrt(n) + 1;
+            for (let i = 0; i < sqrtNum; i += 2)
+                if (n % i == 0) return false;
+            return true;
+        }
     }
 
     hashify(string) {
@@ -25,8 +45,11 @@ class HashMap {
     add(key, val) {
         if (this.loadFactor() > .75) this.grow();
         let index = this.getIndex(this.hashify(key), this.capactiy);
-        this.table[index] = new KeyValuePair(key, val);
-        this.elements++;
+        if (!this.table[index]) {
+            this.elements++;
+            this.table[index] = [];
+        }
+        this.table[index].push(new KeyValuePair(key, val));
         return this;
     }
 
@@ -34,11 +57,15 @@ class HashMap {
         let s = "[ ";
         for (let i = 0; i < this.table.length - 1; i++) {
             if (this.table[i]) {
-                s += `${this.table[i]}, `;
+                this.table[i].forEach(value => s += `${value.value}, `);
             }
         }
         if (this.table[this.table.length - 1]) {
-            s += `${this.table[this.table.length - 1]} `;
+            for (let i = 0; i < this.table[this.table - 1].length - 1; i++) {
+                s += `${this.table[this.table.length - 1][i].value}, `;
+            }
+            let a = this.table[this.table.length - 1];
+            s += `${a[a.length - 1]} `;
         }
         s += "]";
         return s;
@@ -46,7 +73,14 @@ class HashMap {
 
     find(key) {
         let index = this.getIndex(this.hashify(key), this.capactiy);
-        return this.table[index];
+        let values = this.table[index];
+        if (values) {
+            for (let i = 0; i < values.length; i++) {
+                let v = values[i];
+                if (v.key == key) return v;
+            }
+        }
+        return null;
     }
 
     isEmpty() {
@@ -63,27 +97,31 @@ class HashMap {
 
     grow() {
         this.capactiy *= 2;
+        this.capactiy = this.closestLargestPrime(this.capactiy);
         for (let i = 0; i < this.table.length; i++) {
             let curVal = this.table[i];
             this.table[i] = undefined;
             if (curVal) {
-                this.add(curVal.key, curVal.value);
+                curVal.forEach(v => {
+                    this.elements--;
+                    this.add(v.key, v.value);
+                });
             }
         }
     }
 
-    addMap(hashmap, overwrite) {
+    addMap(hashmap) {
         if (hashmap instanceof HashMap) {
             let table = hashmap.table;
             for (let i = 0; i < table.length; i++) {
                 if (table[i]) {
                     let nextIndex = this.getIndex(this.hashify(table[i].key), this.capactiy);
-                    if (this.table[nextIndex] && overwrite) {
-                        this.table[nextIndex] = new KeyValuePair(table[i].key, table[i].value);
-                        this.elements++;
+                    if (this.table[nextIndex]) {
+                        this.table[nextIndex].push(new KeyValuePair(table[i].key, table[i].value));
                     }
                     else if (!this.table[nextIndex]) {
-                        this.table[nextIndex] = new KeyValuePair(table[i].key, table[i].value);
+                        this.table[nextIndex] = [];
+                        this.table[nextIndex].push(new KeyValuePair(table[i].key, table[i].value));
                         this.elements++;
                     }
                 }
@@ -92,8 +130,36 @@ class HashMap {
         }
     }
 
+    setSize(capactiy) {
+        if (capactiy < this.elements) throw new RangeError("capacity given is less than element count");
+        capactiy = this.closestLargestPrime(capactiy);
+        this.capactiy = capactiy;
+        for (let i = 0; i < this.table.length; i++) {
+            let curVal = this.table[i];
+            this.table[i] = undefined;
+            if (curVal) {
+                curVal.forEach(v => {
+                    this.elements--;
+                    this.add(v.key, v.value);
+                });
+            }
+        }
+        this.table.length = capactiy;
+    }
+
     loadFactor() {
         return this.elements / this.capactiy;
+    }
+
+    selectKeys(...keys) {
+        let values = [];
+        for (let i = 0; i < keys.length; i++) {
+            let k = this.find(keys[i]);
+            if (k) {
+                values.push(k.value);
+            }
+        }
+        return values;
     }
 }
 
@@ -107,10 +173,12 @@ class KeyValuePair {
     }
 }
 
-let hashmap = new HashMap(10);
-let hashmap2 = new HashMap(3);
-hashmap2.add("myKey", 42).add("aKey", "foo");
-hashmap.addMap(hashmap2);
-hashmap.add("20", 50)
-console.log(hashmap + "");
-console.log(hashmap.loadFactor());
+let hashmap = new HashMap(3);
+hashmap.add("myKey", 42).add("aKey", "foo");
+hashmap.add("20", 50);
+hashmap.add("test", "hello");
+hashmap.grow();
+hashmap.grow();
+hashmap.setSize(12);
+console.log(hashmap.selectKeys("myKey", "aKey", "20", "test", "hello"));
+console.log(hashmap.table);
