764590f12f2aca4f3deb9ae79ccc8f2a2fda402c
EmilChoparinov <emilchoparinov@gmail.com>
2018-01-05 15:34:21 -0800

    is full

---
+++ chapter-15/is-full.js
@@ -0,0 +1,15 @@
+let reference = require("../chapter-11/reference-class");
+let BST = reference.BST;
+let BTNode = reference.BTNode;
+
+function isFull(node) {
+    if (!node) return true;
+    if (!node.left && !node.right) return true;
+    if (node.left && node.right) return isFull(node.left) && isFull(node.right);
+    return false;
+}
+
+let fullTree = new BST();
+fullTree.add(40).add(35).add(50).add(25).add(37);
+
+console.log(isFull(fullTree.root));
